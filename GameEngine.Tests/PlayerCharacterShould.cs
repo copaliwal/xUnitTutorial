eae658dc864273db0b29753f5d4600d71913ece2
Chandrashekhar Paliwal <copaliwal@gmail.com>
2020-05-17 12:13:43 +0530

    003 Numeric Asserts

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -77,5 +77,32 @@ namespace GameEngine.Tests
 
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
         }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Equal(100, sut.Health);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth_NotEqualExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.NotEqual(0, sut.Health);
+        }
+
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.Sleep(); // Expect increase between 1 to 100 inclusive
+
+            //Assert.True(sut.Health >= 101 && sut.Health <= 200);
+            Assert.InRange(sut.Health, 101, 200);
+        }
     }
 }
