a4571d71f727500b867fd7e7da1563d100e89806
Chandrashekhar Paliwal <copaliwal@gmail.com>
2020-05-17 12:27:33 +0530

    006 Collection Asserts

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -113,5 +113,53 @@ namespace GameEngine.Tests
             //Assert.NotNull(sut.Nickname); //To verify the value is not null
             Assert.Null(sut.Nickname);
         }
+
+        [Fact]
+        public void HaveALongBow()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Contains("Long Bow", sut.Weapons);
+        }
+
+        [Fact]
+        public void NotHaveAStaffOfWonder()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.DoesNotContain("Staff Of Wonder", sut.Weapons);
+        }
+
+        [Fact]
+        public void HaveAtLeastOneKindOfSword()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            //Return a value indicating whether a specified substring occurs within the string
+            Assert.Contains(sut.Weapons, weapon => weapon.Contains("Sword"));
+        }
+
+        [Fact]
+        public void HaveAllExpectedWeapons()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            var expectedWeapons = new[]
+            {
+                "Long Bow",
+                "Short Bow",
+                "Short Sword"
+            };
+
+            Assert.Equal(expectedWeapons, sut.Weapons);
+        }
+
+        [Fact]
+        public void HaveNoEmptyDefaultWeapons()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.All(sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));
+        }
     }
 }
