7ef2831e74d4bf23192f9c9c245aeef96661b71a
Chandrashekhar Paliwal <copaliwal@gmail.com>
2020-05-17 14:12:19 +0530

    016 Sharing Context Across Test Classes

---
+++ GameEngine.Tests/GameStateCollection.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    //     ICollectionFixture: Used to decorate xUnit.net test classes and collections to indicate a test which
+    //     has per-test-collection fixture data. An instance of the fixture data is initialized
+    //     just before the first test in the collection is run, and if it implements IDisposable,
+    //     is disposed after the last test in the collection is run. To gain access to the
+    //     fixture data from inside the test, a constructor argument should be added to
+    //     the test class which exactly matches the TFixture.
+
+    [CollectionDefinition("GameState collection")]
+    public class GameStateCollection : ICollectionFixture<GameStateFixture> {}
+
+}
